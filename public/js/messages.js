a8caaa3664c616cde3061009df15150189ce5bf4
Mishal Islam <mishal.islam01@gmail.com>
2014-12-11 18:57:30 +0000

    user login notification

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,11 +1,17 @@
-var socket = io();
+var socket = io.connect('/');
 
+// user enters a message
 $('form').submit(function(){
   socket.emit('chat message', $('#m').val());
   $('#m').val('');
   return false;
 });
 
+// user receives a broadcast message
 socket.on('chat message', function(msg){
   $('#messages').append($('<li>').text(msg));
 });
+
+socket.on('login', function(login_notification){
+  $('#messages').append($('<li>').text(login_notification));
+})
--- src/chatController.js
+++ src/chatController.js
@@ -1,11 +1,13 @@
 var socket = function(io) {
   io.on('connection', function(socket){
     
-    console.log('a user is connected');
+    io.emit('login', 'User connects')
 
     socket.on('disconnect', function(){
       console.log('user disconnected')
     })
+
+    // server receives a message and then broadcasts that message out
 
     socket.on('chat message', function(msg){
       io.emit('chat message', msg)
