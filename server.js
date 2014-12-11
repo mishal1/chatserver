94e487653acfa11d90a333459846cb2d4c0ce41a
Nick Dyer <nbdyer@gmail.com>
2014-12-11 12:46:30 +0000

    add websocket server

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+var socket = require('./src/socketController.js')(io);
 
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response){
-    response.render('socket');
+    response.render('socket.ejs');
 });
 
 server.listen(3000, function(){
---
+++ src/socketController.js
@@ -0,0 +1,8 @@
+var socket = function(io){
+
+  io.on('connection', function(){
+    console.log('Client connected!');
+  });
+};
+
+module.exports = socket;
