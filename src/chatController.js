f5dafec672092e92937442b7d775a07016c8825e
Nick Dyer <nbdyer@gmail.com>
2014-12-11 16:03:26 +0000

    Add emitting events

--- src/chatController.js
+++ src/chatController.js
@@ -1,9 +1,16 @@
 var socket = function(io) {
   io.on('connection', function(socket){
+    
     console.log('a user is connected');
+
     socket.on('disconnect', function(){
       console.log('user disconnected')
     })
+
+    socket.on('chat message', function(msg){
+      console.log('message: ' + msg);
+    });
+
   });
 }
 
