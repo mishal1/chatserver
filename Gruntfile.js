af6a086ddc146a3afd287c465bcdfc9454366870
Nick Dyer <nbdyer@gmail.com>
2014-12-11 11:46:32 +0000

    Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt){
+grunt.initConfig({
+  pkg: grunt.file.readJSON('package.json'),
+  mocha_casperjs: {
+    options: {
+      timeout: 5000,
+      color: false
+    },
+    files: {
+      src: ['test/**/*.js']
+    }
+  },
+  express: {
+    options:{},
+    dev: {
+      options: {
+        script: './server.js'
+      }
+    }
+  },
+  mochacli: {
+    options: {
+        require: ['chai'],
+        reporter: 'spec',
+        bail: true
+    },
+    all: ['spec/*.js']
+  }
+});
+
+grunt.loadNpmTasks('grunt-mocha-casperjs')
+grunt.loadNpmTasks('grunt-express-server')
+grunt.loadNpmTasks('grunt-mocha-cli')
+
+grunt.registerTask('default',['express','mocha_casperjs','mochacli'])
+
+}
