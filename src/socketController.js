ba768df7e2d67a12474e72c78280f8b3a6aa0f50
Mishal Islam <mishal.islam01@gmail.com>
2014-12-11 15:29:45 +0000

    client can disconnect

--- src/socketController.js
+++ src/socketController.js
@@ -1,7 +1,17 @@
 var socket = function(io){
 
-  io.on('connection', function(){
+  io.on('connection', function(socket){
     console.log('Client connected!');
+    
+
+    socket.on('messages', function(data){
+      console.log(data);
+    });
+
+    socket.on('disconnect', function(){
+      console.log('Client disconnected!');
+    });
+
   });
 };
 
