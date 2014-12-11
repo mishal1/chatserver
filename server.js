ca4d2127505b47048cf47eb02745b2a9ade13845
Nick Dyer <nbdyer@gmail.com>
2014-12-11 15:38:44 +0000

    Serve HTML page for chatserver

--- server.js
+++ server.js
@@ -7,13 +7,18 @@ var socket = require('./src/socketController.js')(io);
 app.set('view engine', 'ejs');
 
 app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
 
 app.get('/', function(request, response){
-    response.render('socket.ejs');
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.get('/sockets', function(request, response){
+  response.render('socket.ejs');
 });
 
 server.listen(3000, function(){
-    console.log("Server listening on port 3000")
+  console.log("Server listening on port 3000")
 });
 
 module.exports = server;
